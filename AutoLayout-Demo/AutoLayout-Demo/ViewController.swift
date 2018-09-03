d91a83698de0d89a75a053f31bcc7a8278ce88aa
MonyneathMol <monyneathmol22@gmail.com>
2018-09-03 09:43:23 +0700

    initial Project

---
+++ AutoLayout-Demo/AutoLayout-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayout-Demo
+//
+//  Created by Mol Monyneath on 9/3/18.
+//  Copyright © 2018 Truemoney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
